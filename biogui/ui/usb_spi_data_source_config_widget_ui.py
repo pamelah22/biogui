8333c8e594526c60f9add1dcde30f53f65463da0
pamelah22 <156887810+pamelah22@users.noreply.github.com>
2025-06-15 15:13:51 -0700

    Update usb_spi_data_source_config_widget_ui.py
    Trying to implement dropdown with devices connected to usb ports

--- biogui/ui/usb_spi_data_source_config_widget_ui.py
+++ biogui/ui/usb_spi_data_source_config_widget_ui.py
@@ -20,25 +20,41 @@ from PySide6.QtWidgets import (QApplication, QComboBox, QFormLayout, QHBoxLayout
     QWidget)
 from . import biogui_rc
 
+
 class Ui_USBDataSourceConfigWidget(object):
-    def setupUi(self, USBDataSourceConfigWidget: QWidget):
-        USBDataSourceConfigWidget.setObjectName("USBDataSourceConfigWidget")
-        USBDataSourceConfigWidget.resize(400, 100)
+    def setupUi(self, USBDataSourceConfigWidget):
+        if not USBDataSourceConfigWidget.objectName():
+            USBDataSourceConfigWidget.setObjectName("USBDataSourceConfigWidget")
+        USBDataSourceConfigWidget.resize(400, 300)
 
         self.verticalLayout = QVBoxLayout(USBDataSourceConfigWidget)
         self.verticalLayout.setObjectName("verticalLayout")
 
-        self.connectButton = QPushButton(USBDataSourceConfigWidget)
-        self.connectButton.setObjectName("connectButton")
-        self.verticalLayout.addWidget(self.connectButton)
+        self.groupBox = QGroupBox(USBDataSourceConfigWidget)
+        self.groupBox.setObjectName("groupBox")
 
-        self.disconnectButton = QPushButton(USBDataSourceConfigWidget)
-        self.disconnectButton.setObjectName("disconnectButton")
-        self.verticalLayout.addWidget(self.disconnectButton)
+        self.formLayout = QFormLayout(self.groupBox)
+        self.formLayout.setObjectName("formLayout")
+
+        self.deviceLabel = QLabel(self.groupBox)
+        self.deviceLabel.setObjectName("deviceLabel")
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.deviceLabel)
+
+        self.deviceComboBox = QComboBox(self.groupBox)
+        self.deviceComboBox.setObjectName("deviceComboBox")
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.deviceComboBox)
+
+        self.autoConnectCheckBox = QCheckBox(self.groupBox)
+        self.autoConnectCheckBox.setObjectName("autoConnectCheckBox")
+        self.formLayout.setWidget(1, QFormLayout.SpanningRole, self.autoConnectCheckBox)
+
+        self.verticalLayout.addWidget(self.groupBox)
 
         self.retranslateUi(USBDataSourceConfigWidget)
+        QMetaObject.connectSlotsByName(USBDataSourceConfigWidget)
 
-    def retranslateUi(self, USBDataSourceConfigWidget: QWidget):
-        _translate = USBDataSourceConfigWidget.tr
-        self.connectButton.setText(_translate("USBDataSourceConfigWidget", "Connect to USB"))
-        self.disconnectButton.setText(_translate("USBDataSourceConfigWidget", "Disconnect"))
+    def retranslateUi(self, USBDataSourceConfigWidget):
+        USBDataSourceConfigWidget.setWindowTitle(QCoreApplication.translate("USBDataSourceConfigWidget", "USB Data Source Config"))
+        self.groupBox.setTitle(QCoreApplication.translate("USBDataSourceConfigWidget", "USB Device Settings"))
+        self.deviceLabel.setText(QCoreApplication.translate("USBDataSourceConfigWidget", "Device:"))
+        self.autoConnectCheckBox.setText(QCoreApplication.translate("USBDataSourceConfigWidget", "Automatically connect to device"))
