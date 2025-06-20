495ed23ae36898dacd19b8a678f8470b44fc7767
pamelah22 <156887810+pamelah22@users.noreply.github.com>
2025-06-19 19:27:06 -0700

    Updated interface_WANDmini.py with import struct

--- interfaces/interface_WANDmini.py
+++ interfaces/interface_WANDmini.py
@@ -15,7 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
+import struct
 import numpy as np
 import time
 from biogui.utils import WANDminiComm
